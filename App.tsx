a6d3fcae9e85028a6337ca8607f8e79eedca6af3
daniells <denniseltravieso01@gmail.com>
2026-05-29 14:44:42 +0400

    instaloHerramientas

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View className="flex-1 bg-slate-900 items-center justify-center p-6">
       <Text className="text-white text-2xl font-bold text-center">
-        ¡MiLink App Funcionando!
+        ¡MiLink App Funcionando nuevo!
       </Text>
       <Text className="text-slate-400 text-sm mt-2 text-center">
         SDK 54 + NativeWind estable
